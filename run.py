e459b5dbb6ffce479f4c47dbd7d07cf7f063ea0f
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-07-29 04:42:06 +0200

    First commit

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-#x1 = np.linspace(0.0, 20.0)
+##x1 = np.linspace(0.0, 20.0)
 #x2 = np.linspace(0.0, 20.0)
 
 ##y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
